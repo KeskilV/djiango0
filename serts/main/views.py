48f5dd3482ddbd2040df96292abbf61a009bf720
KeskilV <gemlab@mail.ru>
2022-07-14 03:05:04 +0300

    qqww

--- serts/main/views.py
+++ serts/main/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse #, HttpResponseNotFound, Http404
 from django.shortcuts import render
 from .models import Diamonds4c
 
-
+#1111
 mainmenu = [{'title': 'Главная', 'url_name': 'home'},
             {'title': 'Наша миссия', 'url_name': 'about'},
             {'title': 'Сертификат соответствия', 'url_name': 'searchsert'},
