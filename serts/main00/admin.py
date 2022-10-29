7662820f1ca62e4f175e3b7e1dad3ad1a8c13e8f
KeskilV <gemlab@mail.ru>
2022-10-29 15:37:06 +0300

    del 00

--- serts/main00/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Diamonds4c
-
-
-admin.site.register(Diamonds4c)
-
-# Register your models here.
--- serts/qrsert00/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class QrsertConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'qrsert'
